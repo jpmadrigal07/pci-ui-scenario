94e0799a80dfd503c5c38af1605e525484b720f5
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-09-19 11:38:19 +0800

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,19 +3,65 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
+
+function getMonthNumber(monthStr: string) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  return months.indexOf(monthStr);
+}
+
+const filterParams = {
+  comparator: (filterLocalDateAtMidnight: Date, cellValue: string) => {
+    var dateAsString = cellValue;
+    if (dateAsString == null) return -1;
+    var dateParts = dateAsString.split(' ');
+    var cellDate = new Date(
+      Number(dateParts[2]),
+      Number(getMonthNumber(dateParts[0])),
+      Number(dateParts[1].replace(',', ''))
+    );
+    if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+      return 0;
+    }
+    if (cellDate < filterLocalDateAtMidnight) {
+      return -1;
+    }
+    if (cellDate > filterLocalDateAtMidnight) {
+      return 1;
+    }
+    return 0;
+  },
+};
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "designation", headerName: "Designation", filter: 'agSetColumnFilter' },
+  { 
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    filter: 'agDateColumnFilter',
+    filterParams: filterParams,
+  },
+  { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter' },
+  { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter' },
+  { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter' },
+  { field: "q_au_2", headerName: "Q (au)", filter: 'agNumberColumnFilter' },
+  { field: "period_yr", headerName: "Period (yr)", filter: 'agNumberColumnFilter' },
+  { field: "i_deg", headerName: "Inclination (deg)", filter: 'agNumberColumnFilter' },
+  { field: "pha", headerName: "Potentially Hazardous", filter: 'agSetColumnFilter' },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, filter: 'agSetColumnFilter' },
 ];
 
 const TITLE = "Near-Earth Object Overview";
@@ -25,6 +71,11 @@ const dateFormat = new Intl.DateTimeFormat('en-US', {
 });
 
 const NeoGrid = (): JSX.Element => {
+  const defaultColDef = useMemo(() => { 
+    return {
+      sortable: true
+    };
+  }, []);
   useEffect(() => {
     document.title = TITLE
   }, []);
@@ -40,6 +91,7 @@ const NeoGrid = (): JSX.Element => {
       <h2>{TITLE}</h2>
       <AgGridReact
         rowData={updatedData}
+        defaultColDef={defaultColDef}
         columnDefs={columnDefs}
         rowGroupPanelShow={'always'}
       />
